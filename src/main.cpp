822e12c64c1c3f0c73e7d910a5e4be8bc310a6bb
Kristoffer Braekken <jaremikb@ulrik.uio.no>
2013-10-21 21:18:22 +0200

    First try at solving problem.

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,19 @@ using namespace arma;
 
 int main(int argc, char* argv[]) {
   SolarSystem mySystem = SolarSystem("../data/sunEarthSystem.dat");
+
+  if (argc != 2) {
+    cout << "Need step size and number of steps." << endl;
+    return 1;
+  }
+
+  double dt = atof(argv[1]);
+  double N = atof(argv[2]);
+
+  for (int i = 0; i < dt*N; i++) {
+    mySystem.advance(dt);
+  }
+
   cout << "Reached end of main." << endl;
   return 0;
 }
